3a8103c4700321e1940cb8380fe50479ee93f433
Illya <katinskyi.illia@lll.kpi.ua>
2024-05-23 15:37:17 +0300

    Add line numbers to the editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,51 @@
+import { useEffect, useRef, useState } from 'react';
+
 export function App() {
+  const [lineNumber, setLineNumber] = useState(1);
+  const mainRef = useRef<HTMLTextAreaElement>(null);
+  const lineNumbersRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (mainRef.current) {
+      mainRef.current.focus();
+    }
+  }, []);
+
+  const handleCodeInputChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    const numberOfLines = event.target.value.split('\n').length;
+    setLineNumber(numberOfLines);
+  };
+
+  const handleScroll = () => {
+    if (lineNumbersRef.current && mainRef.current) {
+      lineNumbersRef.current.scrollTop = mainRef.current.scrollTop;
+    }
+  };
+
   return (
     <div className="container">
       <h1 className="title">Brainfuck Editor</h1>
       <div className="tools">
         <div className="editor">
           <div className="code-input">
+            <div className="line-numbers" ref={lineNumbersRef}>
+              {Array.from({ length: lineNumber }, (_, i) => (
+                <div key={i} className="number">
+                  {i + 1}
+                </div>
+              ))}
+            </div>
             <textarea
               id="code-input"
               placeholder="Write your code here..."
+              ref={mainRef}
+              onChange={handleCodeInputChange}
+              onScroll={handleScroll}
             ></textarea>
-            <button id="run-button"></button>
+            <button className="button" id="run-button"></button>
+            <button className="button" id="clear-button"></button>
           </div>
           <div className="editor-blocks">
             <div className="input-container">
